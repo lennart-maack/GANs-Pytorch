cfbd37f3090a47d3198b1861543e29668019d505
Lennart <LennartMa.19@gmail.com>
2021-11-03 21:25:16 +0100

    Clean up IVUS_hd5_to_Image file

--- utils/IVUS_hd5_to_Image.py
+++ utils/IVUS_hd5_to_Image.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 def img_to_folder(image, img_name, dir, resize):
     if resize:
-        res = cv2.resize(image, dsize=(256,256))
+        res = cv2.resize(image, dsize=(260,260))
     img = Image.fromarray(res.astype(np.uint8))
     img.save(f'{dir}/images/{img_name}')
 
@@ -75,19 +75,19 @@ def h5py_to_image_json(test_patients, cv_sets, resize, test_dir, train_dir):
                             
                             if gname in cv_sets[0] and sub_group_name == "pullback01":
                                 train_counter_0 = train_counter_0 + 1
-                                img_to_folder(image, img_name, train_dir, resize)
+                                #img_to_folder(image, img_name, train_dir, resize)
                                 train_json_list_append(label, train_labels, img_name)
                                 train_create_json_file(train_labels, train_dir)
                 
                             if gname in cv_sets[1] and sub_group_name == "pullback01":
                                 train_counter_1 = train_counter_1 + 1
-                                img_to_folder(image, img_name, train_dir, resize)
+                                #img_to_folder(image, img_name, train_dir, resize)
                                 train_json_list_append(label, train_labels, img_name)
                                 train_create_json_file(train_labels, train_dir)
 
                             if gname in cv_sets[2] and sub_group_name == "pullback02":
                                 train_counter_2 = train_counter_2 + 1
-                                img_to_folder(image, img_name, train_dir, resize)
+                                #img_to_folder(image, img_name, train_dir, resize)
                                 train_json_list_append(label, train_labels, img_name)
                                 train_create_json_file(train_labels, train_dir)
                             
@@ -132,7 +132,7 @@ def h5py_to_image_csv(test_patients, cv_sets, test_dir, train_dir):
                                 test_counter += 1
 
 
-                            if gname in cv_sets[0]:
+                            if gname in cv_sets[0] and sub_group_name == "pullback01":
 
                                 img_to_folder(image, img_name, train_dir, resize=True)
 
@@ -144,7 +144,7 @@ def h5py_to_image_csv(test_patients, cv_sets, test_dir, train_dir):
                                     train_df.loc[train_df["image"]== img_name, "target"] = 0
                                 train_counter = train_counter + 1
 
-                            if gname in cv_sets[1]:
+                            if gname in cv_sets[1] and sub_group_name == "pullback02":
 
                                 img_to_folder(image, img_name, train_dir, resize=True)
 
@@ -156,7 +156,7 @@ def h5py_to_image_csv(test_patients, cv_sets, test_dir, train_dir):
                                     train_df.loc[train_df["image"]== img_name, "target"] = 0
                                 train_counter = train_counter + 1
 
-                            if gname in cv_sets[2]:
+                            if gname in cv_sets[2] and sub_group_name == "pullback02":
 
                                 img_to_folder(image, img_name, train_dir, resize=True)
 
@@ -174,77 +174,9 @@ def h5py_to_image_csv(test_patients, cv_sets, test_dir, train_dir):
     print("train counter: ", train_counter)
     print("counter all: ", counter)
 
-    test_df.to_csv(os.path.join(test_dir, "test.csv"), index=False, header=True)
+    #test_df.to_csv(os.path.join(test_dir, "test.csv"), index=False, header=True)
     train_df.to_csv(os.path.join(train_dir, "train.csv"), index=False, header=True)
 
-def get_h5py_information(cv_sets, leave_out_patients):
-
-    counter = 0
-    test_counter = 0
-    train_counter = 0
-    leave_out_counter = 0
-    positive_counter = 0
-    negative_counter = 0
-    positive_counter_cv0 = 0
-    positive_counter_cv1 = 0
-    positive_counter_cv2 = 0
-    negative_counter_cv0 = 0
-    negative_counter_cv1 = 0
-    negative_counter_cv2 = 0
-    error_counter = 0
-
-    with h5py.File(filename) as h5_file:
-        # Walk through all groups, extracting datasets
-        for gname, group in h5_file.items():
-            for sub_group_name, sub_group in group.items():
-                for dname, ds in sub_group.items():
-                    if dname == "images":
-                        for i in range(len(ds)):
-                            if group[sub_group_name]["labels"][i][0]:
-                                label = 1
-                                positive_counter = positive_counter + 1
-                                if gname in cv_sets[0]:
-                                    positive_counter_cv0 = positive_counter_cv0 + 1
-                                elif gname in cv_sets[1]:
-                                    positive_counter_cv1 = positive_counter_cv1 + 1
-                                elif gname in cv_sets[2]:
-                                    positive_counter_cv2 = positive_counter_cv2 + 1
-                                else:
-                                    error_counter = error_counter + 1
-                            else:
-                                label = 0
-                                negative_counter = negative_counter + 1
-                                if gname in cv_sets[0]:
-                                    negative_counter_cv0 = negative_counter_cv0 + 1
-                                elif gname in cv_sets[1]:
-                                    negative_counter_cv1 = negative_counter_cv1 + 1
-                                elif gname in cv_sets[2]:
-                                    negative_counter_cv2 = negative_counter_cv2 + 1
-                                else:
-                                    error_counter = error_counter + 1
-
-                            image = group[sub_group_name]["images"][i]
-                            img_name = f'{gname}_{sub_group_name}_{i}_' + '{0:05d}'.format(counter) + '.png'
-                            if gname in test_patients:
-                                test_counter = test_counter + 1
-                            elif leave_out_patients is not None and gname in leave_out_patients:
-                                leave_out_counter = leave_out_counter + 1
-                            else:
-                                train_counter = train_counter + 1
-                            counter = counter + 1
-
-    print("test: ", test_counter)
-    print("train: ", train_counter)
-    print("leave out: ", leave_out_counter)
-    print("positive: ", positive_counter)
-    print("negative: ", negative_counter)
-    print("positive_cv0: ", positive_counter_cv0)
-    print("positive_cv1: ", positive_counter_cv1)
-    print("positive_cv2: ", positive_counter_cv2)
-    print("negative_cv0: ", negative_counter_cv0)
-    print("negative_cv1: ", negative_counter_cv1)
-    print("negative_cv2: ", negative_counter_cv2)
-    print("errors: ", error_counter, "Needs to be equal to test size")
 
 if __name__ == "__main__":
 
@@ -268,17 +200,17 @@ if __name__ == "__main__":
     cv_sets_10 = [["patient003"], ["patient002"], ["patient001"]]
 
     #dir_test = r"\test"
-    dir_train = r"G:\My Drive\Projektarbeit_ResearchProject\datasets\IVUS\IVUS_resized\train_val\256\5"
+    dir_train = r"G:\My Drive\Projektarbeit_ResearchProject\datasets\IVUS\IVUS_resized\train_val\260\2"
 
-    # h5py_to_image_csv(
-    #     test_patients, 
-    #     cv_sets=cv_sets_10, 
-    #     test_dir=dir_test, 
-    #     train_dir=dir_train)
-
-    h5py_to_image_json(
-        test_patients=test_patients,
-        cv_sets=cv_sets_10,
-        resize=True,
-        test_dir=None,
+    h5py_to_image_csv(
+        test_patients, 
+        cv_sets=cv_sets_10, 
+        test_dir=None, 
         train_dir=dir_train)
+
+    # h5py_to_image_json(
+    #     test_patients=test_patients,
+    #     cv_sets=cv_sets_10,
+    #     resize=True,
+    #     test_dir=None,
+    #     train_dir=dir_train)
