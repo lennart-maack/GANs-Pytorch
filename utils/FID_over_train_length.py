d4e8ca7c75664a95ff7e58a1c61dc6640de0f639
Lennart <LennartMa.19@gmail.com>
2021-12-04 21:37:16 +0100

    Create file/function to plot FID over train dur

---
+++ utils/FID_over_train_length.py
@@ -0,0 +1,61 @@
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def create_x_values(result) -> list:
+
+    x_values = []
+    for i in range(len(result)):
+        x_values.append(i*600)
+
+    return x_values
+
+
+def create_FID_over_nkimgs_plot(training_session_dir, output_dir, name):
+
+    """
+    Creates a .png file with the FID score plottet over the number of kimgs seen while training.
+
+    training_session_dir: Directory to the folder of one or multiple training sessions - each training session has! to have a metrics.csv included which has as column 'fid50k_full'
+
+    output_dir: Directory where the png file should be saved
+
+    name: to name the .png file uniquely
+
+    """
+
+    frames = []
+    for _, dirs, _ in os.walk(training_session_dir):
+        for subdir in dirs:
+                if os.path.exists(os.path.join(training_session_dir, subdir, "metrics.csv")):
+                    frames.append(pd.read_csv(os.path.join(training_session_dir, subdir, "metrics.csv")))
+
+    result = pd.concat(frames)
+
+    x_values = create_x_values(result)
+
+    y_values = result["fid50k_full"]
+
+    fig = plt.figure()
+    ax=fig.add_subplot()
+    ax.plot(x_values, y_values, color="C0")
+
+    ax.set(xlabel="Number k imgs", ylabel="FID", title="FID over kimgs seen by GAN")
+    ax.set_ylim([50, 500])
+
+    ax.grid()
+
+    plt.savefig(os.path.join(output_dir, f"FID_over_nkimgs_{name}.png"))
+
+
+
+
+if __name__ == "__main__":
+    
+    name = "StyleGAN2_50"
+    training_session_dir = r""
+
+    output_dir = r"G:\My Drive\Projektarbeit_ResearchProject\experiments\BUS_classification\FID_over_nkimgs\STYLEGAN2"
+    
+    create_FID_over_nkimgs_plot(training_session_dir, output_dir, name)
